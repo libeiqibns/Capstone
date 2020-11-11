66c7e4bda7ea0b9dee59e2b07fa4abef9790f24d
Beiqi Li <beiqi.li@mail.utoronto.ca>
2020-11-11 08:01:41 +0800

    minor change

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import codecs, json
+import json
 
 DATA_SIZE = 10
 SAMPLE_SIZE = 10
@@ -20,13 +20,13 @@ def create_json_file(filename):
         randomInts = generate_int(DATA_SIZE)
         data_array = np.vstack((data_array, randomInts))
     
-    with open(filename, 'w') as integer_data:
-        json.dump(data_array.tolist(),integer_data)
+    with open(filename, 'w') as data_file:
+        json.dump(data_array.tolist(),data_file)
 
 
 def decode_json_file(filename):
-    with codecs.open(filename, 'r') as integer_data:
-        data = integer_data.read()
+    with open(filename, 'r') as data_file:
+        data = data_file.read()
         #print(data)
         json_data = json.loads(data)
         return json_data
